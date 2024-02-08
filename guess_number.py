d4597df80ffee0a3275d08a5ddb2595074b22236
Miguel Perez <migeperezjr@gmail.com>
2024-02-07 21:29:50 -0600

    Added the option for the user to quit or to keep guessing

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,14 @@
 number = 10
-
+guess = 11
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+while guess != number:
+    guess = input("What number am I thinking of? (enter Q to quit) ")
+    if guess.upper() == "Q":
+        print(f"The number was {number}.")
+        break
+    elif int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        print(f"Sorry! Try again!")
